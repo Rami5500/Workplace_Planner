b765c6649c96164185eb508cfc3c60869bcf5ea8
Rami5500 <Ramih5500@outlook.com>
2024-03-13 22:30:07 +0000

    images_added

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
-                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                <div class="col-lg-5 d-none d-lg-block bg-register-image" style="background-image: url('register_image.jpg'); background-size: cover;"></div>
                     <div class="col-lg-7">
                         <div class="p-5">
                             <div class="text-center">
@@ -91,7 +91,7 @@
                                 
                             </form>
                             <hr>
-                            
+
                             <!-- <div class="text-center">
                                 <a class="small" href="forgot-password.php">Forgot Password?</a>
                             </div> -->
